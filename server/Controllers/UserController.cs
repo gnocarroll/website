e28f69bd901977a32d66cfffcea8c21b69bddd14
gnocarroll <gocarrol@purdue.edu>
2025-05-31 12:39:18 -0500

    reorganize project without full VS Studio 2022 scaffolding and move to VS Code, scaffolding was messed up and seemed hard to fix whatever was wrong

---
+++ server/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using website.Server.Models.DTOs;
+using website.Server.Models.Entities;
+
+namespace website.Server.Controllers;
+
+public class UserController : WebsiteControllerBase
+{
+    [HttpGet]
+    [Route("all")]
+    public async Task<ActionResult<IEnumerable<UserDTO>>> GetAll()
+    {
+        List<User> users = new();
+
+        users.Add(new User { Id = 1, FirstName = "George", LastName = "O'Carroll"});
+
+        return Ok(from user in users select UserDTO.FromUser(user));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserDTO>>> Get()
+    {
+        return await GetAll();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<UserDTO>> Get(int id)
+    {
+        return Ok(UserDTO.FromUser(
+            new User { Id = 1, FirstName = "George", LastName = "O'Carroll" }
+        ));
+    }
+}
---
+++ server/Models/DTOs/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using website.Server.Models.Entities;
+
+namespace website.Server.Models.DTOs;
+
+public class UserDTO
+{
+    public required int Id;
+
+    public required string FirstName;
+    public required string LastName;
+
+    public required string Bio;
+
+    public static UserDTO FromUser(User user)
+    {
+        return new UserDTO {
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Bio = user.Bio,
+        };
+    }
+}
